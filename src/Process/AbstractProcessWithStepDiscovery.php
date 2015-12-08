b158096e99f143da2be6062b9f08425fd6164aa1
Manuel de Ruiter <info@intowebdevelopment.nl>
2015-12-08 14:03:14 +0100

    Try to detect infinite loops (caused by recursion) automatically

--- src/Process/AbstractProcessWithStepDiscovery.php
+++ src/Process/AbstractProcessWithStepDiscovery.php
@@ -6,10 +6,15 @@
 namespace IntoWebDevelopment\WorkflowBundle\Process;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use IntoWebDevelopment\WorkflowBundle\Exception\InfiniteRecursionStepDiscoveryException;
 use IntoWebDevelopment\WorkflowBundle\Step\StepInterface;
 
 abstract class AbstractProcessWithStepDiscovery extends AbstractProcess implements ProcessInterface
 {
+    private $processedSteps = array();
+    // @TODO: Make this configurable.
+    private $processedStepThreshold = 20;
+
     public function __construct()
     {
         parent::__construct();
@@ -48,6 +53,7 @@ abstract class AbstractProcessWithStepDiscovery extends AbstractProcess implemen
 
         // Add our first call.
         $steps->set($currentStep->getName(), $currentStep);
+        $this->logStep($currentStep->getName());
 
         /**
          * @var     $nextStep       StepInterface
@@ -56,18 +62,56 @@ abstract class AbstractProcessWithStepDiscovery extends AbstractProcess implemen
         foreach ($currentStep->getNextSteps() as $nextStep) {
             // Add our next step.
             $steps->set($nextStep->getName(), $nextStep);
+            $this->logStep($nextStep->getName());
 
             if (count($nextStep->getNextSteps()) > 0) {
                 foreach ($nextStep->getNextSteps() as $nextSubStep) {
                     // Check if we already have an item with the same class key.
                     if (false === $this->stepCollection->containsKey($nextSubStep->getName())) {
-                        $this->iterateDistinctlyOverSteps($nextSubStep, $steps);
+                        if ($this->isThresholdReachedForStep($nextSubStep->getName())) {
+                            throw new InfiniteRecursionStepDiscoveryException(sprintf(
+                                "The threshold of %d was reached while trying to detect an infinite loop. The step that caused this exception: %s, current step: %s",
+                                $this->processedStepThreshold,
+                                $nextSubStep->getName(),
+                                $currentStep->getName()
+                            ));
+                        }
+
+                        if (!$steps->containsKey($nextSubStep->getName())) {
+                            $this->iterateDistinctlyOverSteps($nextSubStep, $steps);
+                        }
                     }
                 }
             }
         }
 
         return $steps;
+    }
+
+    /**
+     * Internal function that helps us keep track of the steps and how many times we
+     *
+     * @internal
+     */
+    private function logStep($stepName)
+    {
+        if (!isset($this->processedSteps[$stepName])) {
+            $this->processedSteps[$stepName] = 0;
+        }
+
+        $this->processedSteps[$stepName]++;
+    }
+
+    /**
+     * Gives us an indication if the recursion goes too wild.
+     *
+     * @internal
+     * @param   string  $stepName
+     * @return  bool
+     */
+    private function isThresholdReachedForStep($stepName)
+    {
+        return isset($this->processedSteps[$stepName]) && $this->processedSteps[$stepName] > $this->processedStepThreshold;
     }
 
     /**
